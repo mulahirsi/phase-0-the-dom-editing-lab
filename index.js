9f64d5fe03bf85534c8acb9122689dce55413181
mulahirsi <mula.hirsi@student.moringaschool.com>
2025-03-18 09:57:29 +0300

    dom editing lab

---
+++ index.js
@@ -0,0 +1,8 @@
+const testVar = "hi"
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
